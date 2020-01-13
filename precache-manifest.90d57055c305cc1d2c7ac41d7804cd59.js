ad86859c07c8a5ca1afdda88e5ec5f3dc7d5cc38
unknown <sizwe.hugo@gmail.com>
2020-01-13 12:41:58 +0200

    Updates

---
+++ precache-manifest.90d57055c305cc1d2c7ac41d7804cd59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38484edef918bd4ac0438c1bbcafce2c",
+    "url": "/My-App/index.html"
+  },
+  {
+    "revision": "f4306362bc4a66bcabe2",
+    "url": "/My-App/static/css/main.f18e4237.chunk.css"
+  },
+  {
+    "revision": "99f04cbd6adbfc8431cf",
+    "url": "/My-App/static/js/2.4de3ba81.chunk.js"
+  },
+  {
+    "revision": "2e7e5a466ae113f07513391c0542446b",
+    "url": "/My-App/static/js/2.4de3ba81.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4306362bc4a66bcabe2",
+    "url": "/My-App/static/js/main.2b82cdf7.chunk.js"
+  },
+  {
+    "revision": "0a5e75606e232cf21fb1",
+    "url": "/My-App/static/js/runtime-main.d43a8935.js"
+  },
+  {
+    "revision": "35fa822f1a07ae0fda65cbf98ec41bbf",
+    "url": "/My-App/static/media/screen-1.35fa822f.jpg"
+  }
+]);
